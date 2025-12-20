9d488ed3dba8219bdf2bbe3110c58f8e163d0ce0
AndersonGL <98903039+AndersonGL@users.noreply.github.com>
2025-12-20 01:50:58 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,12 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  const formatTime = (date) => {
-    return date.toLocaleTimeString("pt-BR", {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    });
-  };
-
-  const formatDate = (date) => {
-    return date.toLocaleDateString("pt-BR", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-  };
-
   return (
-    <>
-      <h1>Hello, Anderson  Gouveia Lignelli</h1>
-      
-      <div className="clock-container">
-        <div className="clock">
-          <div className="time">{formatTime(time)}</div>
-          <div className="date">{formatDate(time)}</div>
-        </div>
-      </div>
-
-   
-
-    </>
+    <div>
+      <h1>Hello, World !</h1>
+    </div>
   );
 }
+
 
 export default App;
