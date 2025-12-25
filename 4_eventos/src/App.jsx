68b906c368617069dfc73e2975df774557c5f319
AndersonGL <98903039+AndersonGL@users.noreply.github.com>
2025-12-25 09:47:50 -0300

    update

---
+++ 4_eventos/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import EventHandlingExemples from './Componentes/EventHandlingExemples';
+
+function App() {
+  
+
+  return <>
+  
+  <EventHandlingExemples />
+  
+  
+  </>;
+
+}
+export default App;
---
+++ 4_eventos/src/Componentes/EventHandlingExemples.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const handleClick = () => {
+  alert("Olá, você clicou no botão!")
+}
+
+const EventHandlingExemples = () => {
+  return (
+    <div>
+      <button onClick={() => alert("oi")}>Clique aqui</button>
+      <button onClick={handleClick}>Clique aqui 2</button>
+    </div>
+  )
+}
+
+export default EventHandlingExemples
+
